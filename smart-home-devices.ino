2802cc191350626ef4e328c9968437dc1531d13d
DankersW <wouter.dankers@skynet.be>
2021-02-08 20:56:02 +0100

    connecting to wifi

--- smart-home-devices.ino
+++ smart-home-devices.ino
@@ -1,21 +1,62 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
-#include "src/radio/radio.h"
 
 #define LED_BUILTIN 2
 
 
-Radio radio;
+class Radio
+{
+    private:
+        struct credentials
+        {
+            const char* ssid = "SaveOurWinters";
+            const char* pwd =  "prettyflyforawifi";
+        } _wifi_cred;
+
+        bool _connect_to_network();
+
+    public:
+        Radio();
+        ~Radio(){}
+};
+
+Radio::Radio()
+{
+    _connect_to_network();
+}
+
+bool Radio::_connect_to_network()
+{
+    WiFi.begin(_wifi_cred.ssid, _wifi_cred.pwd);
+    while (WiFi.status() != WL_CONNECTED){ delay(250); }
+}
+
 
 void setup()
 {
-    // setup radio layer
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH); 
-    radio = Radio();
+    
+    Radio radio = Radio();
+
+    blink_sequence();
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, radio.get_state()); 
+    Serial.println("test");
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW); 
+}
+
+void blink_sequence()
+{
+    for (int i=0; i<=5; i++)
+    {
+        digitalWrite(LED_BUILTIN, LOW); 
+        delay(200);
+        digitalWrite(LED_BUILTIN, HIGH); 
+        delay(200);
+    }
 }
