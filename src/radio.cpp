9d19241ad8a305895b34508631ba70e30b29a4b8
DankersW <wouter.dankers@skynet.be>
2021-02-12 17:59:01 +0100

    Pubsub intro

--- src/radio.cpp
+++ src/radio.cpp
@@ -2,11 +2,12 @@
 
 Radio::Radio()
 {
-    _mqtt_client(_espClient);
+    //_mqtt_client(this->_espClient);
 }
 
 void Radio::begin()
 {
+    //_mqtt_client(WiFiClient _espClient);
     connect_to_network();
     connect_to_mqtt_broker();
 }
@@ -21,11 +22,15 @@ bool Radio::connect_to_network()
 bool Radio::connect_to_mqtt_broker()
 {
     _mqtt_client.setServer(_mqtt_info.broker, _mqtt_info.port);
-    _mqtt_client.setCallback(Radio::callback);
+
+    _mqtt_client.setCallback([this](char* topic, byte* payload, unsigned int length) {
+        this->mqtt_msg_received(topic, payload, length);
+    });
+
     while (!_mqtt_client.connected()){ delay(250); }
     return true;
 }
 
-void Radio::callback(char* topic, byte* payload, unsigned int length)
+void Radio::mqtt_msg_received(char* topic, byte* payload, unsigned int length)
 {
 }
--- src/radio.h
+++ src/radio.h
@@ -19,18 +19,18 @@ class Radio
             const int port = 1883;
         } _mqtt_info;
 
-        WiFiClient _espClient;
+        //WiFiClient _espClient;
         PubSubClient _mqtt_client;
 
         bool connect_to_network();
         bool connect_to_mqtt_broker();
+
+        void mqtt_msg_received(char* topic, byte* payload, unsigned int length);
         
     public:
         Radio();
         ~Radio(){}
         void begin();
-        void callback(char* topic, unsigned char* payload, unsigned int length);
-
 };
 
 #endif
