46928f51a97cbd94fc123dc38a63834461f500b1
DankersW <wouter.dankers@skynet.be>
2021-02-18 22:26:30 +0100

    structre serializer

---
+++ src/serializer.h
@@ -0,0 +1,23 @@
+#ifndef SERIALIZER_H
+#define SERIALIZER_H
+
+#include "types.h"
+
+class Serializer
+{
+private:
+    /* data */
+public:
+    Serializer(/* args */);
+    ~Serializer();
+};
+
+Serializer::Serializer(/* args */)
+{
+}
+
+Serializer::~Serializer()
+{
+}
+
+#endif
